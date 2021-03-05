c567fa51e92de63511d441d65bba4d968572fe43
WD$ <wyntonmdavis@gmail.com>
2021-03-05 17:56:27 -0500

    repo update

---
+++ client/src/components/Form/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function Form(props) {
+    return (
+        <div className="container">
+            <form>
+                <div className="form-group">
+                    <label htmlFor="search"> Search
+                    </label>
+                    <input 
+                        onChange={props.handleInputChange}
+                        value={props.search}
+                        name="search"
+                        type="text"
+                        className="form-control"
+                        placeholder="Search for a book"
+                        id="search"
+                    />
+                    <button onClick={props.handleFormSubmit} className="btn btn-dark mt-3 mb-5">
+                        Search
+                    </button>
+                </div>
+            </form>
+        </div>
+    );
+}
+
+export default Form;
