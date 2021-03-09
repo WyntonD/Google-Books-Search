fb60ee5b24223a732084298c060d62dcd9572696
WD$ <wyntonmdavis@gmail.com>
2021-03-09 16:25:24 -0500

    project pictures

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -5,8 +5,8 @@ import "./style.css";
 function Jumbotron() {
     return (
         <div className="jumbotron text-center">
-            <h1>(React) Google Books Search</h1>
-            <h3>Search for and Save Books of Interest</h3>
+            <h1>Google Books Search</h1>
+            <h3>Search for and save any book of interest with google books.</h3>
         </div>
     );
 }
